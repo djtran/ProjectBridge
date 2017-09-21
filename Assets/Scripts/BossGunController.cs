f6d1e34b819d12944c9aac1b2b89e9d082168167
pappas <pappak@amazon.com>
2017-09-21 16:07:52 -0400

    Boss stuff

---
+++ Assets/Scripts/BossGunController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossGunController : MonoBehaviour {
+
+	public GameObject gun1;
+	public GameObject gun2;
+	public GameObject gun3;
+	public GameObject bulletPrefab;
+	public float coolDownInSeconds = .2f;
+	public float damage = 1.0f;
+
+	private bool bAlternate;
+	private float lastShot;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void Shoot()
+	{
+		if (Time.time >= lastShot + coolDownInSeconds)
+		{
+			if (gun1 != null && gun2 != null)
+			{
+				if (bAlternate)
+				{
+					Fire(gun1);
+					Fire(gun3);
+				}
+				else
+				{
+					Fire(gun2);
+					Fire(gun3);
+				}
+			}
+			else
+			{
+				if(gun1 != null)
+				{
+					Fire(gun1);
+					Fire(gun3);
+				}
+				else
+				{
+					Fire(gun2);
+					Fire(gun3);
+				}
+			}
+			bAlternate = !bAlternate;
+			lastShot = Time.time;
+		}
+	}
+
+	private void Fire(GameObject gun)
+	{
+		var fireDirection = gun.transform.forward;
+		var bullet = (GameObject)Instantiate(bulletPrefab, gun.transform.position, gun.transform.rotation);
+		if (bullet.GetComponent<BulletDamage>() == null)
+		{
+			bullet.AddComponent<BulletDamage>();
+		}
+		bullet.GetComponent<BulletDamage>().damageOnHit = damage;
+		bullet.GetComponent<Rigidbody>().velocity = fireDirection * 10.0f;
+		Destroy(bullet, 3.0f);
+
+	}
+}
