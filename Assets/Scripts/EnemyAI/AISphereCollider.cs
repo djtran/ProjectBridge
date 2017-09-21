1f374ab1d10dd0edb493d973b7e9913f6cefceda
Wang <wyliew@186590d27897.ant.amazon.com>
2017-09-21 17:44:52 -0400

    Add BossAI to spherecollider

--- Assets/Scripts/EnemyAI/AISphereCollider.cs
+++ Assets/Scripts/EnemyAI/AISphereCollider.cs
@@ -11,6 +11,7 @@ public class AISphereCollider : MonoBehaviour
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.tag == "Player") {
 			transform.parent.gameObject.GetComponent<AIStateMachine>().fpsTarget = other.transform;
+			transform.parent.gameObject.GetComponent<BossAI>().fpsTarget = other.transform;
 			Debug.Log ("Entered");
 		}
 			
